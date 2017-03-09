c6ab7999520b441a3a8ebb560b7f029e6ba49f52
fromm <fromm@cs.tum.edu>
2017-03-09 14:43:37 +0100

    InstrumentMethodWrapper modified

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,6 +2,12 @@ import edu.tum.uc.jvm.declassification.Declassifier;
 
 public class Test {
 	public static void main(String[] args) {
+		int o = 13;
+		System.out.println("Before O: "+Integer.toBinaryString(o));
+		int b = (o >> 1) & 1;
+		System.out.println("Afer O: "+Integer.toBinaryString(b));
+	}
+	public static void m(){
 		int i = 54;
 		Object o = new Object();
 		Declassifier.declassify(o);
